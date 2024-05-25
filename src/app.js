3dc9d7b5e76d7485e1f4d50bcdbaf730c60f8196
ManarArabi <manar.araby.ma@gmail.com>
2024-05-25 19:27:17 +0300

    chore(error-handler): add basic error handler

--- src/app.js
+++ src/app.js
@@ -2,11 +2,16 @@ import express from 'express'
 import dotenv from 'dotenv'
 import swaggerUi from 'swagger-ui-express'
 import { swaggerDocument } from '../config/swagger.js'
+import appRouter from './router.js'
+import { errorHandler } from './common/middlewares/error-handler.js'
 
 dotenv.config()
 
 const app = express()
 
+app.use(express.json())
+app.use(appRouter)
 app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
+app.use(errorHandler)
 
 export default app
---
+++ src/common/middlewares/error-handler.js
@@ -0,0 +1,32 @@
+import httpStatus from 'http-status'
+import _ from 'lodash'
+
+const { INTERNAL_SERVER_ERROR, BAD_REQUEST } = httpStatus
+
+export const errorHandler = (error, req, res, next) => {
+  const response = {
+    status: INTERNAL_SERVER_ERROR,
+    meta: { error },
+    message: 'Something Went Wrong'
+  }
+
+  if (!_.isNil(error.status)) {
+    response.status = error.status
+  }
+
+  if (!_.isNil(error.message)) {
+    response.message = error.message
+  }
+
+  if (!_.isNil(error.meta)) {
+    response.meta = error.meta
+  }
+
+  if (error.isJoi) {
+    response.status = BAD_REQUEST
+    response.message = 'Request body is not valid'
+  }
+
+  console.error(error)
+  return res.status(response.status).json(response)
+}
